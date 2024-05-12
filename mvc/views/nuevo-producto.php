23077b2107e1e229b80bb3dbbfb5b1ed51c908cc
Erik Salinas <salinas.erik2002@gmail.com>
2024-05-12 18:08:52 -0300

    cambios nuevos

--- mvc/views/nuevo-producto.php
+++ mvc/views/nuevo-producto.php
@@ -8,11 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<<<<<<< HEAD:mvc/views/menu.html
-    <title>Menu</title>
-=======
     <title>Panel</title>
->>>>>>> a4991946e3e2deefaaf5f1207c4bd2a64dafdf77:mvc/views/nuevo-producto.php
     <link rel="stylesheet" href="../public/css/styles.css" />
 </head>
 <body>
